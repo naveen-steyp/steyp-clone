3bf96d61ff28669718ce2047c2b56904698c1c30
naveennissar <naveennissar15@gmail.com>
2022-08-02 18:13:07 +0530

    Updates

---
+++ precache-manifest.0fa904e48f1802f194bfcdb8d02a2e45.js
@@ -0,0 +1,594 @@
+self.__precacheManifest = [
+  {
+    "revision": "2287281edd546a8e5c62",
+    "url": "/steyp-clone/static/css/main.0753179b.chunk.css"
+  },
+  {
+    "revision": "2287281edd546a8e5c62",
+    "url": "/steyp-clone/static/js/main.2287281e.chunk.js"
+  },
+  {
+    "revision": "f48ed7d98bc049281a4b",
+    "url": "/steyp-clone/static/js/1.f48ed7d9.chunk.js"
+  },
+  {
+    "revision": "39f05787b5a544427664",
+    "url": "/steyp-clone/static/css/2.3a2f87c2.chunk.css"
+  },
+  {
+    "revision": "39f05787b5a544427664",
+    "url": "/steyp-clone/static/js/2.39f05787.chunk.js"
+  },
+  {
+    "revision": "73516e9d173ecdcb3a83",
+    "url": "/steyp-clone/static/js/runtime~main.73516e9d.js"
+  },
+  {
+    "revision": "794ba695ae5f558dd670d1f07e9f74ee",
+    "url": "/steyp-clone/static/media/star.794ba695.svg"
+  },
+  {
+    "revision": "05a992a6515fc70c547726e7fffc4aff",
+    "url": "/steyp-clone/static/media/steyp-logo.05a992a6.svg"
+  },
+  {
+    "revision": "efdecf14c68988ec8760be9c52f8bafd",
+    "url": "/steyp-clone/static/media/arrow.efdecf14.svg"
+  },
+  {
+    "revision": "2b120fddbaa9a301a5e675f5e5b8f792",
+    "url": "/steyp-clone/static/media/linkedin-white.2b120fdd.svg"
+  },
+  {
+    "revision": "85602043f106d45188264b3112338566",
+    "url": "/steyp-clone/static/media/calendar.85602043.svg"
+  },
+  {
+    "revision": "651ecac1eaffc990b2bb7f0fe558f2f4",
+    "url": "/steyp-clone/static/media/bx-play-circle.651ecac1.svg"
+  },
+  {
+    "revision": "3159fbd9d99a2ea4bc9373cd38b33db2",
+    "url": "/steyp-clone/static/media/UI_Engineer.3159fbd9.jpg"
+  },
+  {
+    "revision": "3facea6fbfab7f096e858810cc6c0bd9",
+    "url": "/steyp-clone/static/media/practiseimage'.3facea6f.svg"
+  },
+  {
+    "revision": "3a02d24badfadde152920fb3383c6481",
+    "url": "/steyp-clone/static/media/bx-layer.3a02d24b.svg"
+  },
+  {
+    "revision": "d6c435d1973c7d3926ae1b1528864fe5",
+    "url": "/steyp-clone/static/media/Emptypractices.d6c435d1.svg"
+  },
+  {
+    "revision": "18a7ed6b132ce69516fdbd622080fcbc",
+    "url": "/steyp-clone/static/media/01_-_Caching_Post_Page.18a7ed6b.jpg"
+  },
+  {
+    "revision": "6784bdfa90884616c2f9a68885985121",
+    "url": "/steyp-clone/static/media/01_-_build_a_website.6784bdfa.jpg"
+  },
+  {
+    "revision": "8809d4efe26b1c305302dd335c12e2d2",
+    "url": "/steyp-clone/static/media/01_-_Create_Travel_App.8809d4ef.jpg"
+  },
+  {
+    "revision": "249b682f78a3f0c74f53d99a98145a59",
+    "url": "/steyp-clone/static/media/close.249b682f.svg"
+  },
+  {
+    "revision": "712e6c31ed427305015f5cb102b2ac42",
+    "url": "/steyp-clone/static/media/top-tick.712e6c31.svg"
+  },
+  {
+    "revision": "d40512695bc89365c5306ca94b9e06c2",
+    "url": "/steyp-clone/static/media/red-tick.d4051269.svg"
+  },
+  {
+    "revision": "75e44f1e2b5ee5a57c296d5cd40773f7",
+    "url": "/steyp-clone/static/media/green-tick.75e44f1e.svg"
+  },
+  {
+    "revision": "8ed1934ce7bcb2cf084deda304617e84",
+    "url": "/steyp-clone/static/media/bottom-tick.8ed1934c.svg"
+  },
+  {
+    "revision": "0d4a0c341fc574b347dcfe412ac70255",
+    "url": "/steyp-clone/static/media/fathima.0d4a0c34.jpg"
+  },
+  {
+    "revision": "33ba14079b2daf479b4f7cd8243b802c",
+    "url": "/steyp-clone/static/media/Backend_Developer_ACsVD3G.33ba1407.jpg"
+  },
+  {
+    "revision": "ad6dafeacb1472f008f49c5d8d0151b6",
+    "url": "/steyp-clone/static/media/DevOps_Engineer.ad6dafea.jpg"
+  },
+  {
+    "revision": "ec82608e9d87ac875fc6f4a1f1233fd2",
+    "url": "/steyp-clone/static/media/Web_Application_Developer_vmaZxha.ec82608e.jpg"
+  },
+  {
+    "revision": "d531ff57cc37e5ae8357ff1a2958c394",
+    "url": "/steyp-clone/static/media/Mobile_Application_Developer.d531ff57.jpg"
+  },
+  {
+    "revision": "dfcc3f835ccb20ecdb6f6bdce15716bd",
+    "url": "/steyp-clone/static/media/ERP_Developer.dfcc3f83.jpg"
+  },
+  {
+    "revision": "8918161b0bdae28c497864f7411120b5",
+    "url": "/steyp-clone/static/media/Data_Scientist.8918161b.jpg"
+  },
+  {
+    "revision": "2f75c5343ef881be6a0641b0d1d9564c",
+    "url": "/steyp-clone/static/media/Robotics_Engineer__1.2f75c534.jpg"
+  },
+  {
+    "revision": "c2afa5533c2f0a51270a018d319ada46",
+    "url": "/steyp-clone/static/media/AI_Engineer.c2afa553.jpg"
+  },
+  {
+    "revision": "45ff522bf20c3e920ce2fccbe8c23f50",
+    "url": "/steyp-clone/static/media/Blockchain_Developer.45ff522b.jpg"
+  },
+  {
+    "revision": "6e5a34f978408fcb13fe757507b64c20",
+    "url": "/steyp-clone/static/media/AR_Engineer.6e5a34f9.jpg"
+  },
+  {
+    "revision": "a5e2adc3ffc6920bfc593ea7544fa0da",
+    "url": "/steyp-clone/static/media/VR_Engineer.a5e2adc3.jpg"
+  },
+  {
+    "revision": "d775acc8d0541056988580c056b3cd25",
+    "url": "/steyp-clone/static/media/IoT_Engineer.d775acc8.jpg"
+  },
+  {
+    "revision": "0ec1bd2e4882b2b90d7586e3a8434db9",
+    "url": "/steyp-clone/static/media/Machine_Learning_Engineer.0ec1bd2e.jpg"
+  },
+  {
+    "revision": "bf88bb90aa253f3542b31cc12d3d246c",
+    "url": "/steyp-clone/static/media/Configure_sublime_text_1.bf88bb90.jpg"
+  },
+  {
+    "revision": "8f8fc889a9739732e20d1de9a01919f6",
+    "url": "/steyp-clone/static/media/Gordita Regular.8f8fc889.otf"
+  },
+  {
+    "revision": "2a23af3d8545de5786416ee382d5d66a",
+    "url": "/steyp-clone/static/media/Gordita Medium.2a23af3d.otf"
+  },
+  {
+    "revision": "78dd25235fa4006f25693d3cd8933469",
+    "url": "/steyp-clone/static/media/Gordita Bold.78dd2523.otf"
+  },
+  {
+    "revision": "efbae562bd651f8773956bbb4295895a",
+    "url": "/steyp-clone/static/media/bismiya-min.efbae562.jpg"
+  },
+  {
+    "revision": "7d8e77ecd45148a236f38295ccabf9e3",
+    "url": "/steyp-clone/static/media/laxmi.7d8e77ec.jpg"
+  },
+  {
+    "revision": "c120dbe8a25d36329fd57af8ea61b8a0",
+    "url": "/steyp-clone/static/media/sanoop-min-min-min_z20uBMu.c120dbe8.jpg"
+  },
+  {
+    "revision": "0d4a0c341fc574b347dcfe412ac70255",
+    "url": "/steyp-clone/static/media/Copy_of_14061-Fath_cEY4FGm.0d4a0c34.jpg"
+  },
+  {
+    "revision": "2b99bceb60b4f9520e057a52e90b5f78",
+    "url": "/steyp-clone/static/media/14355-Joel_Lee_Roy-min.2b99bceb.jpeg"
+  },
+  {
+    "revision": "5d823a7dff594f782e87dd18433e9509",
+    "url": "/steyp-clone/static/media/Faheem_C_M-min.5d823a7d.jpg"
+  },
+  {
+    "revision": "7b560c075d551f810d71bea67683e2a3",
+    "url": "/steyp-clone/static/media/sidebar-menu.7b560c07.svg"
+  },
+  {
+    "revision": "5be946fbd0d281fb27ba4f608dd9352c",
+    "url": "/steyp-clone/static/media/arrow black.5be946fb.svg"
+  },
+  {
+    "revision": "c1d7c887161378eccfebf77450cd263a",
+    "url": "/steyp-clone/static/media/phone.c1d7c887.svg"
+  },
+  {
+    "revision": "fa0a3b54337d2cf677f7bffe64e8c36a",
+    "url": "/steyp-clone/static/media/programs.fa0a3b54.svg"
+  },
+  {
+    "revision": "e67f755a8a63bf8b31c31e041b8b7589",
+    "url": "/steyp-clone/static/media/company.e67f755a.svg"
+  },
+  {
+    "revision": "e36dd5e66154027513f513d99aa9c585",
+    "url": "/steyp-clone/static/media/community.e36dd5e6.svg"
+  },
+  {
+    "revision": "e1a94e16cc4b89193a544d2f63a9aa66",
+    "url": "/steyp-clone/static/media/spotlight-background.e1a94e16.png"
+  },
+  {
+    "revision": "2503704f675eea1d039fed1138df4c31",
+    "url": "/steyp-clone/static/media/spotlight-mammookka.2503704f.png"
+  },
+  {
+    "revision": "e5a9c01d1e191cdc6aa7907aabf3c068",
+    "url": "/steyp-clone/static/media/introduction-logo.e5a9c01d.png"
+  },
+  {
+    "revision": "70efdbb6ca5c2fa9632d059f8933af42",
+    "url": "/steyp-clone/static/media/side-image.70efdbb6.png"
+  },
+  {
+    "revision": "ebf2f4da5249c8a3486c19655c9fcf44",
+    "url": "/steyp-clone/static/media/who-can-banner.ebf2f4da.png"
+  },
+  {
+    "revision": "3ea5ca3907346c6c18d71de26af3c563",
+    "url": "/steyp-clone/static/media/techie-club.3ea5ca39.png"
+  },
+  {
+    "revision": "86b068e8eddb3cf06f252d6785d8b31b",
+    "url": "/steyp-clone/static/media/one.86b068e8.svg"
+  },
+  {
+    "revision": "3721c60efa0a46e2973c3ff8528ad39c",
+    "url": "/steyp-clone/static/media/two.3721c60e.svg"
+  },
+  {
+    "revision": "976f14b47e91658589cc5ec0f5be7827",
+    "url": "/steyp-clone/static/media/club.976f14b4.svg"
+  },
+  {
+    "revision": "88caee5227754e070fa725af3a9ccbc4",
+    "url": "/steyp-clone/static/media/hub.88caee52.svg"
+  },
+  {
+    "revision": "29fa43c6aa4fa22e112b5c4e5f2c16ea",
+    "url": "/steyp-clone/static/media/degree.29fa43c6.svg"
+  },
+  {
+    "revision": "1324792d84effed39b67384c4441c139",
+    "url": "/steyp-clone/static/media/grey-logo.1324792d.svg"
+  },
+  {
+    "revision": "65cbbc86e6917e8120e62e69402ea1e6",
+    "url": "/steyp-clone/static/media/instagram-color.65cbbc86.svg"
+  },
+  {
+    "revision": "1c1a5113b48fd51d2c025d8dac398e40",
+    "url": "/steyp-clone/static/media/facebook-color.1c1a5113.svg"
+  },
+  {
+    "revision": "2be730f021691f609f4b386dcb8b17b1",
+    "url": "/steyp-clone/static/media/twitter-color (1).2be730f0.svg"
+  },
+  {
+    "revision": "c685467a35399175544bc4e1d861d601",
+    "url": "/steyp-clone/static/media/linkedin-color.c685467a.svg"
+  },
+  {
+    "revision": "d920728bd17285d27d7e90152138d628",
+    "url": "/steyp-clone/static/media/youtube-color.d920728b.svg"
+  },
+  {
+    "revision": "cd079a20c88c232a978f27572b2e618f",
+    "url": "/steyp-clone/static/media/bx-coin-stack.cd079a20.svg"
+  },
+  {
+    "revision": "c6b970ca25a8b30cee5df30191a31858",
+    "url": "/steyp-clone/static/media/bx-plus.c6b970ca.svg"
+  },
+  {
+    "revision": "4a2902d91749893a13522a6121083eb7",
+    "url": "/steyp-clone/static/media/messages.4a2902d9.svg"
+  },
+  {
+    "revision": "ae569c80deb27a5b9b0dcde8dd0728e6",
+    "url": "/steyp-clone/static/media/polygon.ae569c80.svg"
+  },
+  {
+    "revision": "7867f2bd4c8e31978cfcb81684f81d72",
+    "url": "/steyp-clone/static/media/menu-icon.7867f2bd.svg"
+  },
+  {
+    "revision": "3ff53aa736484c2be0b8375daab2a8a8",
+    "url": "/steyp-clone/static/media/dashboard.3ff53aa7.svg"
+  },
+  {
+    "revision": "a2ae543f30a7031d3bac4844e28bc8a9",
+    "url": "/steyp-clone/static/media/free.a2ae543f.svg"
+  },
+  {
+    "revision": "f092db6a9e61e1a046bd3f1de26e763e",
+    "url": "/steyp-clone/static/media/challenge.f092db6a.svg"
+  },
+  {
+    "revision": "839eb701fac734a3a7d2210ae8e6ee8e",
+    "url": "/steyp-clone/static/media/talk.839eb701.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/steyp-clone/static/media/linkedinsocial.4b475fb3.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/steyp-clone/static/media/twittersocial.c9f12008.svg"
+  },
+  {
+    "revision": "c1e769605e4686003967900cb433dc65",
+    "url": "/steyp-clone/static/media/facebooksocial.c1e76960.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/steyp-clone/static/media/instagramsocail.fc23b902.svg"
+  },
+  {
+    "revision": "a51ab7ed010a415170cd93a2a29d96dd",
+    "url": "/steyp-clone/static/media/youtubesocial.a51ab7ed.svg"
+  },
+  {
+    "revision": "edaf0f819678b90ba49d5ab1c40f0840",
+    "url": "/steyp-clone/static/media/explore-white-arrow.edaf0f81.svg"
+  },
+  {
+    "revision": "0db16999ee5d510c2757cf43a204c445",
+    "url": "/steyp-clone/static/media/decor.0db16999.svg"
+  },
+  {
+    "revision": "05aca5c02194f8541914696dcf52f34c",
+    "url": "/steyp-clone/static/media/sideaf.05aca5c0.svg"
+  },
+  {
+    "revision": "95581cb34b30184c6be26b9f9bff7064",
+    "url": "/steyp-clone/static/media/techschoolingimg.95581cb3.svg"
+  },
+  {
+    "revision": "bbb9cd6899ed12bf19093a28466578ec",
+    "url": "/steyp-clone/static/media/greenarrow_KMnh8MC.bbb9cd68.svg"
+  },
+  {
+    "revision": "9ff7e3fce31ca79d16169f4033d122b0",
+    "url": "/steyp-clone/static/media/dots.9ff7e3fc.svg"
+  },
+  {
+    "revision": "3b22abd76c57c61703165582c87ff57a",
+    "url": "/steyp-clone/static/media/lock.3b22abd7.svg"
+  },
+  {
+    "revision": "77107c3a79457bfaf57bafc863e8c1ee",
+    "url": "/steyp-clone/static/media/03_-_Data_Types_and_Operations__1.77107c3a.jpg"
+  },
+  {
+    "revision": "7910c5cd1496ea18f5d21a5f77b84e74",
+    "url": "/steyp-clone/static/media/09_-_Datetime_module__1.7910c5cd.jpg"
+  },
+  {
+    "revision": "42f8add949caa90b31492b318f296204",
+    "url": "/steyp-clone/static/media/React_JS_Second_Assessment.42f8add9.jpg"
+  },
+  {
+    "revision": "3a6e71538b4ccdd23136e1ff4537a287",
+    "url": "/steyp-clone/static/media/React_JS_First_Assessment.3a6e7153.jpg"
+  },
+  {
+    "revision": "1ccdd2ef6b18c36b498a46939a0d660e",
+    "url": "/steyp-clone/static/media/Bootstrap_First_Assessment.1ccdd2ef.jpg"
+  },
+  {
+    "revision": "ff1f38c1abf2db94acfa8a0a49acaaa9",
+    "url": "/steyp-clone/static/media/06_-_Control_flow_statements__1.ff1f38c1.jpg"
+  },
+  {
+    "revision": "080716a0174663f40e1bcf77a7ffb9e3",
+    "url": "/steyp-clone/static/media/1_Technology_Thumbnail.080716a0.jpg"
+  },
+  {
+    "revision": "5da5ca7fed8ccd161893df25392d414f",
+    "url": "/steyp-clone/static/media/bx-time.5da5ca7f.svg"
+  },
+  {
+    "revision": "ef254f4e0ba3504e7896930a3269d510",
+    "url": "/steyp-clone/static/media/star.ef254f4e.ef254f4e.svg"
+  },
+  {
+    "revision": "889eacd3a11da89ec618697360524e68",
+    "url": "/steyp-clone/static/media/DeviceImage.889eacd3.jpg"
+  },
+  {
+    "revision": "04380385632c58b9b10c1c5d5657ec32",
+    "url": "/steyp-clone/static/media/OsImage.04380385.jpg"
+  },
+  {
+    "revision": "3dd946552d02db6ebbd58ac9e46c132f",
+    "url": "/steyp-clone/static/media/KeyImage.3dd94655.jpg"
+  },
+  {
+    "revision": "0f0c91daf0838b39f44c004cfd29e007",
+    "url": "/steyp-clone/static/media/MouseImg.0f0c91da.jpg"
+  },
+  {
+    "revision": "3c664d33f0b7bfec2114a5dd25ac51a5",
+    "url": "/steyp-clone/static/media/What_isInternet_MLb1IZv.3c664d33.jpg"
+  },
+  {
+    "revision": "a2e03a618f51f941169ab10140006ed0",
+    "url": "/steyp-clone/static/media/Sublime_features__1_rM9Giup.a2e03a61.jpg"
+  },
+  {
+    "revision": "0b5a2eef27d92604f893803d2e7c0b23",
+    "url": "/steyp-clone/static/media/install_sublime_on_mac_os.0b5a2eef.jpg"
+  },
+  {
+    "revision": "3b38a8c391d719dc0fd416b57b7b2ad3",
+    "url": "/steyp-clone/static/media/Configure_Sublime_on_ubuntu__2.3b38a8c3.jpg"
+  },
+  {
+    "revision": "e8d09262b1f3940d96a5d90e66529b4b",
+    "url": "/steyp-clone/static/media/Sublime_features.e8d09262.jpg"
+  },
+  {
+    "revision": "a8077f5e924f1d91f7af51bd550411ec",
+    "url": "/steyp-clone/static/media/subshortcut.a8077f5e.jpg"
+  },
+  {
+    "revision": "93eada0bb4ed042f821f2d9cde01e2ec",
+    "url": "/steyp-clone/static/media/subwindows.93eada0b.jpg"
+  },
+  {
+    "revision": "589cbc23306b9695737ac2bbd1b05817",
+    "url": "/steyp-clone/static/media/sublimewindows.589cbc23.jpg"
+  },
+  {
+    "revision": "d7e6ee11e5d3e5d227667b009482aff0",
+    "url": "/steyp-clone/static/media/Unzipping_on_windows.d7e6ee11.jpg"
+  },
+  {
+    "revision": "73ab8c6caccbcb1eb16db9c7b90c8259",
+    "url": "/steyp-clone/static/media/zipping_on_windows.73ab8c6c.jpg"
+  },
+  {
+    "revision": "c6d536e2c0fb50bf7399e77d1efd2a70",
+    "url": "/steyp-clone/static/media/01_-_Install_file_manager.c6d536e2.jpg"
+  },
+  {
+    "revision": "1df8ac41519f3d65717e667417fc9696",
+    "url": "/steyp-clone/static/media/02_-_Creating_a_folder.1df8ac41.jpg"
+  },
+  {
+    "revision": "86fbae9d6ccbb6558729bb1867304d41",
+    "url": "/steyp-clone/static/media/03_-_Installing_code_editor_and_creating_files.86fbae9d.jpg"
+  },
+  {
+    "revision": "0d8f2dfc87b272becf59c24981d47e34",
+    "url": "/steyp-clone/static/media/04_-_How_to_compress_a_folder.0d8f2dfc.jpg"
+  },
+  {
+    "revision": "2fa8f7a630cc7655893362aac75f63a9",
+    "url": "/steyp-clone/static/media/05_-_Editor_Options.2fa8f7a6.jpg"
+  },
+  {
+    "revision": "9913b9661553f7b6146638b992760e0c",
+    "url": "/steyp-clone/static/media/06_-_Setting_chrome_as_default_browser.9913b966.jpg"
+  },
+  {
+    "revision": "eaf4d56e7ff9e2c81dc538a2a9b7a68b",
+    "url": "/steyp-clone/static/media/07_-_Download_and_show_images.eaf4d56e.jpg"
+  },
+  {
+    "revision": "a11ba63e205d2b5341785348806201de",
+    "url": "/steyp-clone/static/media/08_-_Uploading_a_practice.a11ba63e.jpg"
+  },
+  {
+    "revision": "b606e9e2a35c405ee80a9b237959b9db",
+    "url": "/steyp-clone/static/media/09_-_Download_assets_and_extract.b606e9e2.jpg"
+  },
+  {
+    "revision": "a0cfb8e09aba9e0edff1d3a8f5be9ca6",
+    "url": "/steyp-clone/static/media/10_-_Connecting_keyboard_in_android_phone.a0cfb8e0.jpg"
+  },
+  {
+    "revision": "56e3d520d4c5934a3ec9b422a4964c56",
+    "url": "/steyp-clone/static/media/bx-search.56e3d520.svg"
+  },
+  {
+    "revision": "fd4ed41fad26c1c886479208ac4542bc",
+    "url": "/steyp-clone/static/media/empty-challenge.fd4ed41f.svg"
+  },
+  {
+    "revision": "cccf2adf7e42eed1321461dd26941d13",
+    "url": "/steyp-clone/static/media/challengeback.cccf2adf.png"
+  },
+  {
+    "revision": "d8f8f4c57ca949555a76da89168bc890",
+    "url": "/steyp-clone/static/media/Alwin_Sunil.d8f8f4c5.jpg"
+  },
+  {
+    "revision": "9146ff4d7d79a05d875fd6739f94e067",
+    "url": "/steyp-clone/static/media/winner.9146ff4d.svg"
+  },
+  {
+    "revision": "c3ed7ac4f37198d758b1a157404dd3f9",
+    "url": "/steyp-clone/static/media/runner.c3ed7ac4.svg"
+  },
+  {
+    "revision": "092b1e7c944d7f41792756813c3053ae",
+    "url": "/steyp-clone/static/media/Muhsin-challenge.092b1e7c.jpeg"
+  },
+  {
+    "revision": "971168c63cb3d98be722c23f6015d1cc",
+    "url": "/steyp-clone/static/media/Adithyan_S_P.971168c6.jpeg"
+  },
+  {
+    "revision": "3c49e25b0464cdc401839fd6c94f6f8a",
+    "url": "/steyp-clone/static/media/Swathi.3c49e25b.jpeg"
+  },
+  {
+    "revision": "1d4cdc973bffd7c863bcb1019dbc185f",
+    "url": "/steyp-clone/static/media/Arshad_khan.1d4cdc97.png"
+  },
+  {
+    "revision": "ccc1007b2811fd734c3258d8a1247bb6",
+    "url": "/steyp-clone/static/media/emoji.ccc1007b.svg"
+  },
+  {
+    "revision": "2278f694505db7f20d469b54fb17449c",
+    "url": "/steyp-clone/static/media/image.2278f694.svg"
+  },
+  {
+    "revision": "5096c79f6fc7b4f84e94260e75e6dab7",
+    "url": "/steyp-clone/static/media/attach.5096c79f.svg"
+  },
+  {
+    "revision": "4ca77a4bf3ea670e5cecb39b242e3966",
+    "url": "/steyp-clone/static/media/mic.4ca77a4b.svg"
+  },
+  {
+    "revision": "7f9b4e548de3990db1d1638a76aeaaba",
+    "url": "/steyp-clone/static/media/avatar.7f9b4e54.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/steyp-clone/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/steyp-clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/steyp-clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/steyp-clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/steyp-clone/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "538d8c78d56fc252d46987a6ca79adc4",
+    "url": "/steyp-clone/static/media/abhinath-S-R.538d8c78.jpeg"
+  },
+  {
+    "revision": "6db52a5f50f0f80ef7b86d927b7dca74",
+    "url": "/steyp-clone/static/media/Vaifav.6db52a5f.jpg"
+  },
+  {
+    "revision": "8bbb03b485eec198ea724d73b9f11383",
+    "url": "/steyp-clone/index.html"
+  }
+];
